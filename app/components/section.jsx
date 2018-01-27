cc3feb7194603fae385581d71b6740b1424404f1
NikolasUntoten <nikolasr@ggaub.com>
2018-01-26 22:20:02 -0800

    Added test featured speaker, reworked meet the team

--- app/components/section.jsx
+++ app/components/section.jsx
@@ -6,14 +6,22 @@ import {StyleSheet, css} from 'aphrodite';
 export class Section extends React.Component {
     render(){
         let title = this.props.title;
-        
+		let borderSize = this.props.border;
+		if (typeof borderSize === 'undefined') {
+			borderSize = '15%';
+		}
+		var style = {
+			paddingLeft:borderSize,
+			paddingRight:borderSize,
+		}
+		
         // No title given
         if (typeof title === 'undefined') {
-            return ( <div className={css(styles.tedsection)}>{this.props.children}</div> );
+            return ( <div style={style} className={css(styles.tedsection)}>{this.props.children}</div> );
         }
         
         return ( 
-            <div className={css(styles.tedsection)}>
+            <div style={style} className={css(styles.tedsection)}>
                 <div className={css(styles.tedsectiontitle)}>{title}</div>
                 {this.props.children}
             </div> 
@@ -25,13 +33,10 @@ export class Section extends React.Component {
 
 const styles = StyleSheet.create({
     tedsection: {
-        width: '70%',
         minHeight: '75vh',
         background: 'white',
-
+		margin:'0px',
         paddingTop: '50px',
-        paddingLeft: '15%',
-        paddingRight: '15%',
         paddingBottom: '50px'
     },
     tedsectiontitle: {
--- app/components/tedprofiles.jsx
+++ app/components/tedprofiles.jsx
@@ -7,56 +7,46 @@ import {Profile} from '../components/profile.jsx';
 
 export class Profiles extends React.Component {
 	render() {
+		
+		let profs = this.props.profiles;
+		
+		if (typeof this.props.team !== 'undefined') {
+			var arr = [];
+			for (var i = 0; i < profs.length; i++) {
+				if (profs[i].team == this.props.team) {
+					arr.push(profs[i]);
+				}
+			}
+			profs = arr;
+		}		
+		
+		var rows = [];
+		
+		for (var i = 0; i < profs.length; i+=3) {
+			var row;
+			if (i+3 < profs.length) {
+				row = profs.slice(i, i+3);
+			} else {
+				row = profs.slice(i);
+			}
+			rows.push(	
+				<tr className={css(styles.tr)}>
+					{row.map(profile => (
+							<td className={css(styles.td)}>
+								<Profile 
+									img={profile.img} 
+									title={profile.name} 
+									role={profile.role}
+								/>
+							</td>
+						))}
+				</tr>);
+		}
+		
 		return (
 			<table frame="void" className={css(styles.table)}>
 				<tbody>
-				<tr className={css(styles.tr)}>
-					<td className={css(styles.td)}>
-						<Profile 
-							img="/app/resources/images/generic.jpg" 
-							title="Jenny Liang" 
-							role="Web Developer" 
-						/>
-					</td>
-					<td className={css(styles.td)}>
-						<Profile 
-							img="/app/resources/images/generic.jpg" 
-							title="Jenny Liang" 
-							role="Web Developer"
-							company="Test Agency"
-						/>
-					</td>
-					<td className={css(styles.td)}>
-						<Profile 
-							img="/app/resources/images/generic.jpg" 
-							title="Name" 
-							role="Role" 
-						/>
-					</td>
-				</tr>
-				<tr className={css(styles.tr)}>
-					<td className={css(styles.td)}>
-						<Profile 
-							img="/app/resources/images/generic.jpg" 
-							title="Name" 
-							role="Role" 
-						/>
-					</td>
-					<td className={css(styles.td)}>
-						<Profile 
-							img="/app/resources/images/generic.jpg"  
-							title="Name" 
-							role="Role" 
-						/>
-					</td>
-					<td className={css(styles.td)}>
-						<Profile 
-							img="/app/resources/images/generic.jpg" 
-							title="Name" 
-							role="Role" 
-						/>
-					</td>
-				</tr>
+					{rows}
 				</tbody>
 			</table>
 		);
