ffa4ceefbf39ef9c38b6d411b6d3f16a26130520
NikolasUntoten <nikolasr@ggaub.com>
2018-02-01 12:28:21 -0800

    rough main page links skeleton

---
+++ src/app/components/linksection.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {StyleSheet, css} from 'aphrodite';
+
+export class LinkSection extends React.Component {
+    render(){
+		var style = {
+			backgroundColor:this.props.bgColor,
+			color:this.props.color,
+		}
+        return (
+			<div style={style} className={css(styles.section)}>
+				<p style={style} className={css(styles.title)}> {this.props.title} </p>
+				{this.props.children}
+			</div>
+		);
+    }
+}
+
+
+
+const styles = StyleSheet.create({
+	section: {
+        minHeight: '50vh',
+		margin:'0px',
+        paddingTop: '50px',
+        paddingBottom: '50px'
+    },
+    title: {
+        fontSize: '3vw',
+        fontWeight: 'bold',
+        fontFamily: 'AvenirBlack',
+		marginTop:'8vw',
+		textAlign: 'center',
+    },
+});
--- src/app/pages/main.jsx
+++ src/app/pages/main.jsx
@@ -6,6 +6,7 @@ import {Link} from 'react-router-dom';
 import {TEDNavbar} from '../components/tednavbar.jsx';
 import {TEDFooter} from '../components/tedfooter.jsx';
 import {Section} from '../components/section.jsx';
+import {LinksSection} from '../components/multilinksection.jsx';
 import {FeaturedSpeaker} from '../components/featuredspeaker.jsx';
 
 export class Home extends React.Component {
@@ -17,11 +18,9 @@ export class Home extends React.Component {
                     Section 1
                 </Section>
                 <Section border='0%'>
-                    <FeaturedSpeaker img="/app/resources/images/generic.jpg" name="Erin Jones" desc="Office of Public Instruction"/>
+					Section 2
                 </Section>
-                <Section border='0%'>
-                    Section 3
-                </Section>
+                <LinksSection border='0%' />
                 <TEDFooter />
             </div>
         );
