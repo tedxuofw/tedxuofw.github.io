40cb44007947f6c9a514cb6dc8faf81db5d94de1
NikolasUntoten <nikolasr@ggaub.com>
2018-03-11 12:59:30 -0700

    Revert "Merge branch 'master' of https://github.com/SohamPardeshi/tedxuofw2018"
    This reverts commit 8b18d44eaecab27006e58334bfd142623e1b4a52, reversing
    changes made to a2e0f8da212f87b41324e9b8f21df2e463be9726.

---
+++ app/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+import { Home } from './pages/main.jsx';
+import { About } from './pages/about.jsx';
+import { Speakers } from './pages/speakers.jsx';
+import { Sponsors } from './pages/sponsors.jsx';
+import { Contacts } from './pages/contacts.jsx';
+import { Attend } from './pages/attend.jsx';
+
+class App extends React.Component {
+	render() {
+		return (
+			<HashRouter>
+				<Switch>
+					<Route exact path='/' component={Home}/>
+					<Route path='/about' component={About}/>
+                    <Route path='/speakers' component={Speakers}/>
+                    <Route path='/sponsors' component={Sponsors}/>
+                    <Route path='/contacts'  component={Contacts}/>
+                    <Route path='/attend'   component={Attend}/>
+                    
+                </Switch>
+			</HashRouter>
+        );
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
---
+++ app/pages/contact.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {TEDNavbar} from '../components/tednavbar.jsx';
+import {TEDFooter} from '../components/tedfooter.jsx';
+import {Section} from '../components/section.jsx';
+
+export class Contact extends React.Component {
+    
+    componentDidMount() {
+        document.title = "Contact - TEDxUofW";
+    }
+    
+	render() {
+                
+		return (
+            <div>
+                <TEDNavbar index='3'/>
+                <Section title='Contact'>
+                   
+                </Section>
+                <TEDFooter />
+            </div>
+        );
+	}
+}
