ac522b5ceec2d0f1e7347e1cc5c381cc77bdec3d
NikolasUntoten <nikolasr@ggaub.com>
2018-04-04 00:06:28 -0700

    Added randomized featured speakers

--- src/app/components/featuredspeakersection.jsx
+++ src/app/components/featuredspeakersection.jsx
@@ -6,29 +6,62 @@ import {StyleSheet, css} from 'aphrodite';
 import {FeaturedSpeaker} from '../components/featuredspeaker.jsx';
 import {TedButton} from '../components/tedbutton.jsx';
 
+const speakers =[ {name:"Kristi Straus", 	desc:"UW Environmental Studies", 	img:"/app/resources/images/kristi-straus.jpg"},
+			{name:"Ansel Santosa", 	desc:"UW Information School", 				img:"/app/resources/images/ansel-santosa.jpg"},
+			{name:"Heather Evans", 	desc:"University of Washington", 			img:"/app/resources/images/heather-evans.jpg"},
+			{name:"Dr. Stephen Bezruchka", desc:"Departments of Health Services and Global Health", img:"/app/resources/images/stephen-bezruchka.jpg"},
+			{name:"John Sinclair", desc:"Songwriter, Producer, and Performer", 	img:"/app/resources/images/john-sinclair.jpg"},
+			{name:"Fauzia Lala", desc:"", 										img:"/app/resources/images/fauzia-lala.jpg"},
+			{name:"Andrea Weatherhead", desc:"", 								img:"/app/resources/images/andrea-weatherhead.jpg"},
+			{name:"Tyler Valentine", desc:"University of Washington", 			img:"/app/resources/images/tyler-valentine.jpg"},
+			{name:"Sarah Mhyre", desc:"UW Department of Oceanography", 			img:"/app/resources/images/sarah-mhyre.jpg"},
+			{name:"Jeffery Lew", desc:"", 										img:"/app/resources/images/jeffery-lew.jpg"},
+			{name:"Izdihar Bailey", desc:"", 									img:"/app/resources/images/izdihar-bailey.jpg"},
+			{name:"Karen Liftin", desc:"", 										img:"/app/resources/images/karen-liftin.jpg"},
+			{name:"Tarah Wheeler", desc:"", 									img:"/app/resources/images/tarah-wheeler.jpg"} 
+			];
+
 export class Speakers extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		
+		this.getSelection = this.getSelection.bind(this);
+		this.state = {table:this.getSelection()};
+	}
+	
+	getSelection() {
+		var table = [];
+		while (table.length < 3) {
+			const num = Math.floor(Math.random() * speakers.length);
+			if (!table.includes(speakers[num])) table.push(speakers[num]);
+		}
+		return table;
+	}
+	
 	render() {
+		var table = this.state.table;
 		return (
 			<div className={css(styles.container)}>
 				<div className={css(styles.headercontainer)}>
 					<p className={css(styles.header)}>Featuring</p>
 				</div>
 				<FeaturedSpeaker 
-					name="Erin Jones"
-					desc="Office of Public Education"
-					img="/app/resources/images/erin-jones.jpg"
+					name={table[0].name}
+					desc={table[0].desc}
+					img={table[0].img}
 					flipped={true}
 				/>
 				<FeaturedSpeaker 
-					name="Jasmine Samy"
-					desc="Council of Islamic American Relations"
-					img="/app/resources/images/generic.jpg"
+					name={table[1].name}
+					desc={table[1].desc}
+					img={table[1].img}
 					flipped={false}
 				/>
 				<FeaturedSpeaker 
-					name="Jeannie Berwick"
-					desc="One Equal Heart Foundation"
-					img="/app/resources/images/generic.jpg"
+					name={table[2].name}
+					desc={table[2].desc}
+					img={table[2].img}
 					flipped={true}
 				/>
 				<TedButton name="View All Speakers" linkTo="/speakers" />
